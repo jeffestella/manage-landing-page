8331f4715eed8a14a75be2839c358f94e7e31b7b
Jeff <jeffrey.estella@gmail.com>
2022-01-22 08:52:46 -0800

    Finish styling SectionHero. Adjust NavBar padding

--- src/components/SectionHero.js
+++ src/components/SectionHero.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import sectionHeroStyles from './SectionHero.module.css';
 
+import CTABtn from './CTABtn';
+import heroGraphics from '../assets/illustration-intro.svg';
+
 const SectionHero = () => {
     return (
         <section className={sectionHeroStyles.container}>
-            Hero Section
+            <div className={sectionHeroStyles.copy}>
+                <h1>Bring everyone together to build better products.</h1>
+                <p>Manage makes it simple for software teams to plan day-to-day tasks while keeping the larger team goals in view.</p>
+                <CTABtn />
+            </div>
+            <div className={sectionHeroStyles.graphics}>
+                <img src={heroGraphics} alt="data graphics" />
+            </div>
         </section>
     );
 }
