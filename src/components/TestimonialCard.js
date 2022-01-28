440e0503a948a87c17d9b7f0fe02e554f2e2c7a0
Jeff <jeffrey.estella@gmail.com>
2022-01-27 18:26:45 -0800

    Adjust styling for TestimonialCard

--- src/components/TestimonialCard.js
+++ src/components/TestimonialCard.js
@@ -4,10 +4,12 @@ import testimonialCardStyles from './TestimonialCard.module.css';
 const TestimonialCard = ({ name, photo, blurb}) => {
     return (
         <article className={testimonialCardStyles.container}>
-            <img src={photo} alt={`${name}'s avatar`} />
-            <div className={testimonialCardStyles.text}>
-                <h1>{name}</h1>
-                <p>{blurb}</p>
+            <div className={testimonialCardStyles.content}>
+                <img src={photo} alt={`${name}'s avatar`} />
+                <div className={testimonialCardStyles.text}>
+                    <h1>{name}</h1>
+                    <p>{blurb}</p>
+                </div>
             </div>
         </article>
     );
