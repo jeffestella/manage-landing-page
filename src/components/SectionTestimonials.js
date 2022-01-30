7083492b8d6f79e533bb9f2bc22ff5464d77c00f
Jeff <jeffrey.estella@gmail.com>
2022-01-30 07:11:00 -0800

    Resize SectionTestimonial and TestimonialCard for mobile

--- src/components/SectionTestimonials.js
+++ src/components/SectionTestimonials.js
@@ -12,24 +12,28 @@ import TestimonialCard from './TestimonialCard';
 const testimonials = [
     {
         name: "Anisha Li",
+        title: "anisha-li",
         photo: avatarAnisha,
         blurb: `"Manage has supercharged our team\'s workflow. The ability to maintain visibility on larger milestones at all times keeps everyone motivated."`
-
+        
     },
     {
         name: "Ali Bravo",
+        title: "ali-bravo",
         photo: avatarAli,
         blurb: `"We have been able to cancel so many other subscriptions since using Manage. There is no more cross-channel confusion and everyone is much more focused."`
-
+        
     },
     {
         name: "Richard Watts",
+        title: "richard-watts",
         photo: avatarRichard,
         blurb: `"Managed allows us to provide structure and process. It keeps us organized and focused. I can\'t stop recommending them to everyone I talk to!"`
-
+        
     },
     {
         name: "Shanai Gough",
+        title: "shanai-gough",
         photo: avatarShanai,
         blurb: `"Their software allows us to track, manage, and collaborate on our projects from anywhere. It keeps the whole team in-sync without being intrusive."`
 
